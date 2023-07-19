38e0f110e9cd85ae8a5d7348371fa68e7cced6b0
Andrei Stanciu <andrei.stanciu@idea-bank.ro>
2023-07-19 09:49:04 +0300

    Initial Commit

---
+++ OnBoarding/OnBoardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingApp.swift
+//  OnBoarding
+//
+//  Created by Andrei Stanciu on 19.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
