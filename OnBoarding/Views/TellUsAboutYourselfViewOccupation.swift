102ef751353498dfee1f22a88e0f28f41471b0af
Andrei Stanciu <andrei.stanciu@idea-bank.ro>
2023-07-20 14:30:26 +0300

    TellMeAboutYourselfOccupationView - screen number 4

---
+++ OnBoarding/Views/TellUsAboutYourselfViewOccupation.swift
@@ -0,0 +1,59 @@
+import SwiftUI
+
+struct TellUsAboutYourselfViewOccupation: View {
+    let occupations = [
+        "Employee",
+        "Pensioner",
+        "Student",
+        "Household",
+        "Investor",
+        "Self-employed"]
+    @State private var selectedOccupation: String?
+    var body: some View {
+        GeometryReader { geometry in
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("What is your main occupation?")
+                        .font(.title3)
+                        .padding(.horizontal, 15)
+                        .padding(.top, 30)
+                    List(selection: $selectedOccupation) {
+                        ForEach(occupations, id:\.self) { occupation in
+                            HStack {
+                                Text(occupation)
+                                    .padding(.horizontal, 15)
+                                Spacer()
+                                if selectedOccupation == occupation {
+                                    Image(systemName: "checkmark")
+                                        .foregroundColor(.blue)
+                                        .padding(.horizontal, 15)
+                                }
+                                
+                            }
+                            
+                        }
+                        .frame(width: geometry.size.width, alignment: .leading)
+                    }
+                    .listStyle(GroupedListStyle())
+                    .scrollContentBackground(.hidden)
+                    .scrollDisabled(true)
+                    .ignoresSafeArea()
+                    Spacer()
+                    SettingsNotification()
+                        .padding(.horizontal, 15)
+                        .padding(.bottom, 15)
+                }
+            }
+        .navigationTitle("Tell Us About Yourself")
+        .navigationDestination(for: NavigationScreen.self) { _ in
+            TellUsAboutYourselfViewIncome()
+        }
+    }
+}
+
+struct TellUsAboutYourselfViewOccupation_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            TellUsAboutYourselfViewOccupation()
+        }
+    }
+}
